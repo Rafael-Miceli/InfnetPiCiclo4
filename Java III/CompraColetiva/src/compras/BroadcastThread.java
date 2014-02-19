e3450407148be6826b7f256215b7c2ecd49f4c12
ricardomsj <rmendes06@hotmail.com>
2014-02-19 18:50:10 -0300

    Update BroadcastThread.java
    Pois antes aparecia o nome e quantidade do produto para todos os clientes, só que a quantidade do produto só pode aparecer para os clientes que abrirem a pagina do produto

--- Java III/CompraColetiva/src/compras/BroadcastThread.java
+++ Java III/CompraColetiva/src/compras/BroadcastThread.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
-//Para enviar as atualizações do Produto
+//Para enviar as atualizaï¿½ï¿½es do Produto
 
 public class BroadcastThread implements Runnable {
 
@@ -27,14 +27,14 @@ public class BroadcastThread implements Runnable {
 		
 		// 1 - Recebe o produto escolhido pelo cliente 
 		// Mais complexo, pois a partir dessa escolha , vai ser aberta a pagina desse produto para o cliente 
-		// e ele vai ficar vendo a atualização da quantidade de produtos
-		// e também vai poder comprar o produto
+		// e ele vai ficar vendo a atualizaï¿½ï¿½o da quantidade de produtos
+		// e tambï¿½m vai poder comprar o produto
 		
 		
-		// 2 - Manda a lista de Produtos para todos os clientes
+		// 2 - Manda a lista dos nomes dos Produtos para todos os clientes
 		for(PrintStream ps : listaBroadcast){
 		   for(Produto p :listaProdutos){
-			   ps.println("Nome : " + p.getNome() + " / Quantidade : " + p.getQuantidade());
+			   ps.println("Nome : " + p.getNome()); //NÃ£o coloquei a quantidade de produtos , pois o cliente sÃ³ vai vÃª-la quando abrir a pÃ¡gina do produto 
 		   }
 		}
 	}
