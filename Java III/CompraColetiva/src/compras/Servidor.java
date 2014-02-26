c0f07d52b22756465b6b8263cd00622dfeb0c444
ricardomsj <rmendes06@hotmail.com>
2014-02-25 21:59:21 -0300

    Update Servidor.java
    Comentário na lista de broadcast

--- Java III/CompraColetiva/src/compras/Servidor.java
+++ Java III/CompraColetiva/src/compras/Servidor.java
@@ -8,13 +8,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-//Servidor
-
 public class Servidor {
-
+	
 	List<Produto> listaProdutos;
 	private int porta;
-	private List<PrintStream> listaBroadcast;
+	private List<PrintStream> listaBroadcast; //tem que ter uma para cada produto ou colocar essa lista na classe Produto
 	private ServerSocket server;
 
 	//________________
@@ -33,29 +31,31 @@ public class Servidor {
 	//________________
 
 	public void executarVendedor(){
-		
+
 		try
 		{
 			server = new ServerSocket(this.porta);
 			System.out.println("Porta 10000 Aberta !");
 			inserirProduto();
-			
+
 			//Fica esperando o cliente
 			while(true){
 				Socket socket = server.accept();
 
-				System.out.println("Nova conexão com o cliente " +   
+				System.out.println("Nova conexÃ£o com o cliente " +   
 						socket.getInetAddress().getHostAddress()
 						);
-				
-				//Para armazenar a saída dos clientes e mandar as atualizações do produto
+
+				//Para armazenar a saÃ­da dos clientes e mandar as atualizaÃ§Ãµes do produto
 				PrintStream ps = new PrintStream(socket.getOutputStream());
 				listaBroadcast.add(ps);
 
-				//Tratamento do recebimento de msg e distribuição
+				//Tratamento do recebimento de msg e distribuiÃ§Ã£o
 				//AQUI EU DELEGO PARA THREAD
+				
 				BroadcastThread broadcastThread = new BroadcastThread(socket , listaBroadcast , listaProdutos);
 				new Thread(broadcastThread).start();
+				
 			}
 		} 
 		catch (IOException e) 
@@ -75,28 +75,29 @@ public class Servidor {
 
 	//________________
 
-	
+
 	public void inserirProduto(){
 
 
 		Scanner s = new Scanner(System.in);
-		
+
 		System.out.println("\nEntre com o total de produtos oferecidos : ");
 		int op = s.nextInt();
 
 		for(int i=1; i <= op; i++){
-			
-			Produto produto = new Produto();
-			
+
+			Produto produto = new Produto(); // GC mata o anterior , compilador faz isso , entende que vai precisar de novas interaÃ§Ãµes a cada for
+
 			System.out.println("\nEntre com o nome do produto");	
 			produto.setNome(s.next());
 			System.out.println("\nEntre com a quantidade de produtos");	
 			produto.setQuantidade(s.nextInt());
 
 			listaProdutos.add(produto);
-			
+
 		}
 		
+
 	}
 
 
@@ -108,5 +109,6 @@ public class Servidor {
 		vendedor.executarVendedor();
 
 	}
+
 
 }
